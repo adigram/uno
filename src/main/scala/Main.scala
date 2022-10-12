8fa0f959a0f9ef275b152de83bc6e16dcf8a8bd7
Adrian <adrianrohde@protonmail.com>
2022-10-12 19:07:07 +0200

    translatet to english

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,20 @@
-@main def main():Unit = println(austeilen(6))
+@main def main():Unit = println(deal(6))
 
-val Farben: List[String] = List[String]("🟥","🟩","🟦","🟨","⬛")
+val Colors: List[String] = List[String]("🟥","🟩","🟦","🟨","⬛")
 
-val Symbole: List[String] = List[String]( "0","1","2","3","4","5","6","7","8","9","🚫","🔃","+2","🌈","+4")
+val Symbols: List[String] = List[String]( "0","1","2","3","4","5","6","7","8","9","🚫","🔃","+2","🌈","+4")
 
-def haende(): List[(Int,Int)]={
+def hands(): List[(Int,Int)]={
    val r = scala.util.Random
-   val ergebnis = List[(Int,Int)]( (r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)),
+   val result = List[(Int,Int)]( (r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)),
    (r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)) )
-   ergebnis
+   result
 }
 
-def austeilen(spieler: Int):String={
+def deal(player: Int):String={
   val r = scala.util.Random
-  (1 until (spieler+1)).map(x => "Hand Spieler "  + x +"\n"+
-   haende().map(k=> Farben.apply(k._1) +" "+Symbole.apply(k._2)).mkString("  |  ")  + "\n").mkString +
-   "Letzte Karte(Stappel) \n| " +   Farben.apply(r.nextInt(3))+" "+Symbole.apply(r.nextInt(8)) +" |"
+  (1 until (player+1)).map(x => "Hand Player "  + x +"\n"+
+   hands().map(k=> Colors.apply(k._1) +" "+Symbols.apply(k._2)).mkString("  |  ")  + "\n").mkString +
+   "Last card(stack) \n| " +   Colors.apply(r.nextInt(3))+" "+Symbols.apply(r.nextInt(8)) +" |"
 }
  
