e0dc78328bae61b4e7a5ec00262aa3a4944855a2
aldidada <darius.eiglsperger@gmx.de>
2022-10-12 19:12:07 +0200

    better Funktion haende2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,17 @@
 @main def main():Unit = println(austeilen(6))
 
-val Farben: List[String] = List[String]("🟥","🟩","🟦","🟨","⬛")
+val colour: List[String] = List[String]("🟥","🟩","🟦","🟨","⬛")
 
-val Symbole: List[String] = List[String]( "0","1","2","3","4","5","6","7","8","9","🚫","🔃","+2","🌈","+4")
+val symbol: List[String] = List[String]( "0","1","2","3","4","5","6","7","8","9","🚫","🔃","+2","🌈","+4")
 
-def haende(): List[(Int,Int)]={
-   val r = scala.util.Random
-   val ergebnis = List[(Int,Int)]( (r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)),
-   (r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)),(r.nextInt(5),r.nextInt(15)) )
-   ergebnis
-}
+val r = scala.util.Random
+
+def haende2():List[(Int,Int)]=(0 until 6).map(k =>(r.nextInt(5),r.nextInt(15))).toList
 
 def austeilen(spieler: Int):String={
   val r = scala.util.Random
   (1 until (spieler+1)).map(x => "Hand Spieler "  + x +"\n"+
-   haende().map(k=> Farben.apply(k._1) +" "+Symbole.apply(k._2)).mkString("  |  ")  + "\n").mkString +
-   "Letzte Karte(Stappel) \n| " +   Farben.apply(r.nextInt(3))+" "+Symbole.apply(r.nextInt(8)) +" |"
+   haende2().map(k=> colour.apply(k._1) +" "+symbol.apply(k._2)).mkString("  |  ")  + "\n").mkString +
+   "Letzte Karte(Stappel) \n| " +   colour.apply(r.nextInt(3))+" "+symbol.apply(r.nextInt(8)) +" |"
 }
  
