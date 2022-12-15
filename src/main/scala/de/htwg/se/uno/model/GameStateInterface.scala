eb6e00d4d386c2268cd3fdb939d7e155ee3e8689
aldidada <darius.eiglsperger@gmx.de>
2022-12-15 17:56:15 +0100

    changes

--- src/main/scala/de/htwg/se/uno/model/GameStateInterface.scala
+++ src/main/scala/de/htwg/se/uno/model/GameStateInterface.scala
@@ -9,21 +9,21 @@ trait GameStateInterface {
   val deck:List[Card]
   val stack:List[Card]
 
-  def handle(e:Event):state
+  def handle(e:Event):GameStateInterface
   def createZeroCards():List[Card]
   def createNormalCards():List[Card]
   def wildCards():List[Card]
-  def createDeck():state
-  def createGame():state
-  def takeCardFromDeck(): state
-  def createPlayers(Namen:List[String]):state
+  def createDeck():GameStateInterface
+  def createGame():GameStateInterface
+  def takeCardFromDeck(): GameStateInterface
+  def createPlayers(Namen:List[String]):GameStateInterface
   def takeCard(n: Int): (List[Card] ,List[Card]) 
-  def nextPlayer(): state
-  def UnoUno(): state
-  def dropCard(chosenCard:Option[Int],unoFlag:Boolean): state
-  def CardCheck(input: Boolean, selection: Int, unoFlag:Boolean) : state
-  def dropLastCard(chosenCard:Option[Int],unoFlag:Boolean): state
-  def dropNormalCard(CardNumber: Int, unoFlag:Boolean): state
-  def chooseColour(colourNumber: Option[Int]): state
+  def nextPlayer(): GameStateInterface
+  def UnoUno(): GameStateInterface
+  def dropCard(chosenCard:Option[Int],unoFlag:Boolean): GameStateInterface
+  def CardCheck(input: Boolean, selection: Int, unoFlag:Boolean) : GameStateInterface
+  def dropLastCard(chosenCard:Option[Int],unoFlag:Boolean): GameStateInterface
+  def dropNormalCard(CardNumber: Int, unoFlag:Boolean): GameStateInterface
+  def chooseColour(colourNumber: Option[Int]): GameStateInterface
 
 }
